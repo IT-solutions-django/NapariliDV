7d866a793be6fd74252e60b126940464233ede69
Пак Сергей <sergegnabri2005@gmail.com>
2024-11-24 10:10:34 +0700

    Add models for projects app

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import (
+    Project, 
+    Category, 
+    Material, 
+    RoofType, 
+    Floor
+)
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin): 
+    list_display = ['name']
+
+
+@admin.register(Material)
+class MaterialAdmin(admin.ModelAdmin): 
+    list_display = ['name']
+
+
+@admin.register(RoofType)
+class RoofTypeAdmin(admin.ModelAdmin): 
+    list_display = ['name']
+
+
+class FloorInline(admin.TabularInline):
+    model = Floor
+    extra = 1
+    verbose_name = 'Этаж'
+    verbose_name_plural = 'Этажи'
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin): 
+    list_display = ['id', 'description', 'square', 'price', 'rooms_quantity']
+
+    inlines = [
+        FloorInline,
+    ] 
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Category(models.Model): 
+    name = models.CharField('Название', max_length=100)
+
+    class Meta: 
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Material(models.Model): 
+    name = models.CharField('Название', max_length=100)
+
+    class Meta: 
+        verbose_name = 'Материал'
+        verbose_name_plural = 'Материалы'
+
+
+class RoofType(models.Model): 
+    name = models.CharField('Название', max_length=100)
+
+    class Meta: 
+        verbose_name = 'Тип кровли'
+        verbose_name_plural = 'Типы кровли'
+
+
+class Project(models.Model): 
+    description = models.TextField('Описание')
+    square = models.FloatField('Площадь', validators=[MinValueValidator(0.0)])
+    price = models.DecimalField('Цена', decimal_places=2, max_digits=12)
+    rooms_quantity = models.SmallIntegerField('Количество комнат', validators=[MinValueValidator(0.0)])
+    category = models.ForeignKey(
+        verbose_name='Категория', 
+        to=Category, 
+        on_delete=models.CASCADE, 
+        related_name='category_projects'
+    )
+    material = models.ForeignKey(
+        verbose_name='Материал', 
+        to=Material, 
+        on_delete=models.CASCADE, 
+        related_name='material_projects'
+    )
+    roof_type = models.ForeignKey(
+        verbose_name='Тип кровли', 
+        to=Material, 
+        on_delete=models.CASCADE, 
+        related_name='roof_type_projects'
+    )
+
+    class Meta: 
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+
+
+class Floor(models.Model): 
+    number = models.SmallIntegerField('Номер', validators=[MinValueValidator(0.0)])
+    square = models.FloatField('Площадь')
+    project = models.ForeignKey(
+        verbose_name='Проект', 
+        to=Project, 
+        on_delete=models.CASCADE, 
+        related_name='floors'
+    )
+
+    class Meta: 
+        verbose_name = 'Этаж'
+        verbose_name_plural = 'Этажи'
+
+
+class ProjectPhoto(models.Model): 
+    image = models.ImageField('Изображение', upload_to='projects', null=True, blank=True)
+    project = models.ForeignKey(verbose_name='Проект', to=Project, on_delete=models.CASCADE, related_name='photos', null=True)
