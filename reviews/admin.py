132b677904402e284ad113f7aa76ff674519f63b
Пак Сергей <sergegnabri2005@gmail.com>
2024-11-24 10:27:22 +0700

    Create TwoGISReview model

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import (
+    TwoGISReview,
+)
 
-# Register your models here.
+
+@admin.register(TwoGISReview)
+class TwoGISReviewAdmin(admin.ModelAdmin): 
+    list_display = ['id', 'username', 'content', 'created_at']
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class TwoGISReview(models.Model): 
+    rate = models.SmallIntegerField('Оценка', validators=[
+        MinValueValidator(1), 
+        MaxValueValidator(5)
+    ])
+    username = models.CharField('Имя пользователя', max_length=100)
+    content = models.TextField('Содержание')
+    created_at = models.DateTimeField('Дата и время публикации')
+
+    class Meta: 
+        verbose_name = 'Отзыв с 2GIS'
+        verbose_name_plural = 'Отзывы с 2GIS'
+        ordering = ['-created_at']
+
+    def __str__(self) -> str: 
+        return self.content[:20]
