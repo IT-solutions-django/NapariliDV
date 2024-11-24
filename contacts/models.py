f405a7f905dcced0cf4527f6598d5f08da8c83ca
Пак Сергей <sergegnabri2005@gmail.com>
2024-11-24 10:23:03 +0700

    Update models for contacts app

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Worker(models.Model): 
+    last_name = models.CharField('Фамилия', max_length=100)
+    first_name = models.CharField('Имя', max_length=100)
+    patronymic = models.CharField('Отчество', max_length=100)
+    role = models.TextField('Роль')
+    photo = models.ImageField('Фото', upload_to='contacts/workers')
+
+    class Meta: 
+        verbose_name = 'Сотрудник'
+        verbose_name_plural = 'Сотрудники'
+
+
+class Request(models.Model): 
+    name = models.CharField('Имя', max_length=50)
+    phone = models.CharField('Телефон', max_length=20)
+    is_closed = models.BooleanField('Обработано', default=False)
+
+    class Meta: 
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
