a3df74f7a5c4e1eda67f784a0c2d2b0eb24e85c2
Пак Сергей <sergegnabri2005@gmail.com>
2024-11-27 14:25:08 +0700

    Add Platform model for reviews

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from .models import (
     Review,
+    Platform
 )
 
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin): 
     list_display = ['id', 'username', 'content', 'created_at']
+
+
+@admin.register(Platform)
+class PlatformAdmin(admin.ModelAdmin): 
+    list_display = ['name']
--- reviews/models.py
+++ reviews/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
+class Platform(models.Model): 
+    name = models.CharField('Название платформы', max_length=100)
+
+    def __str__(self) -> str: 
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Платформа'
+        verbose_name_plural = 'Платформы с отзывами'
+
+
 class Review(models.Model): 
     rate = models.SmallIntegerField('Оценка', validators=[
         MinValueValidator(1), 
@@ -10,6 +21,7 @@ class Review(models.Model):
     username = models.CharField('Имя пользователя', max_length=100)
     content = models.TextField('Содержание')
     created_at = models.DateTimeField('Дата и время публикации')
+    platform = models.ForeignKey(verbose_name='Платформа', to=Platform, on_delete=models.CASCADE)
 
     class Meta: 
         verbose_name = 'Отзыв'
@@ -18,3 +30,4 @@ class Review(models.Model):
 
     def __str__(self) -> str: 
         return self.content[:20]
+    
