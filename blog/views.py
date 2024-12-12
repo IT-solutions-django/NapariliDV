f61548e1e557492a08a500a9f1226b813ce75901
Пак Сергей <sergegnabri2005@gmail.com>
2024-12-12 20:58:58 +0700

    Add article page

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from django.views import View 
 
 
-
 class BlogView(View): 
     template_name = 'blog/blog.html'
+
+    def get(self, request): 
+        return render(request, self.template_name)
+    
+
+class ArticleView(View): 
+    template_name = 'blog/article.html'
 
     def get(self, request): 
         return render(request, self.template_name)
