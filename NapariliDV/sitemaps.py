a8060422ad52da36c9ebd5275adf4acb52422033
Пак Сергей <sergegnabri2005@gmail.com>
2025-01-21 17:51:32 +0700

    Fix sitemap

--- NapariliDV/sitemaps.py
+++ NapariliDV/sitemaps.py
@@ -3,8 +3,19 @@ from django.shortcuts import reverse
 from projects.models import Project
 from blog.models import Article
 
+class Site:
+    domain = 'naparilidv.com'
+    
 
-class HomeViewSitemap(Sitemap): 
+class BaseSitemap(Sitemap):
+    protocol = 'https' 
+    
+    def get_urls(self, site=None, **kwargs):
+        site = Site()
+        return super(BaseSitemap, self).get_urls(site=site, **kwargs)
+
+
+class HomeViewSitemap(BaseSitemap): 
     priority = 1 
 
     def items(self): 
@@ -16,7 +27,7 @@ class HomeViewSitemap(Sitemap):
         return reverse(item)
 
 
-class StaticViewSitemap(Sitemap):
+class StaticViewSitemap(BaseSitemap):
     priority = 0.9
 
     def items(self):
@@ -32,21 +43,21 @@ class StaticViewSitemap(Sitemap):
         return reverse(item)
     
 
-class ProjectViewSitemap(Sitemap): 
+class ProjectViewSitemap(BaseSitemap): 
     priority = 0.8 
 
     def items(self): 
         return Project.objects.all()
     
 
-class BlogViewSitemap(Sitemap): 
+class BlogViewSitemap(BaseSitemap): 
     priority = 0.7 
 
     def items(self): 
         return Article.objects.all()
     
 
-class OtherStatisViewSitemap(Sitemap): 
+class OtherStatisViewSitemap(BaseSitemap): 
     priority = 0.75  
 
     def items(self): 
