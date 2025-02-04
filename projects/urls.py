84387843f645f27f0d27cf8e18a502077fd574f7
Пак Сергей <sergegnabri2005@gmail.com>
2025-02-04 17:16:56 +0700

    Change project ID to project slug in URLS

--- projects/urls.py
+++ projects/urls.py
@@ -7,5 +7,5 @@ app_name = 'projects'
 
 urlpatterns = [
     path('catalog', CatalogView.as_view(), name='catalog'), 
-    path('<int:id>/', ProjectView.as_view(), name='project'),
+    path('<slug:project_slug>/', ProjectView.as_view(), name='project'),
 ]
